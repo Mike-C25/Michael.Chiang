faf1c6e7fd1b58275fbc355e05b0e7bb4d091f04
Michael Chiang <michael.chiang25@gmail.com>
2017-11-27 13:38:09 -0500

    changed format of page + css changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,35 @@ $(document).ready(function() {
     const rightSide = document.querySelector('.r');
     rightSide.classList.add('t');
 
-    $('.link').each(function(fadeInDiv) {
-        $(this).delay(fadeInDiv * 500).addClass('f');
-    }).delay(1000);
+    const fadeInLinks = function() {
+    	$('.link').addClass('f');
+        // $('.link').each(function(fadeInDiv) {
+        //     $(this).delay(fadeInDiv * 1500).addClass('f');
+        //     // console.log("going");
+        // }).delay(2000);
+    }
 
+
+    $(".link").on('click', function(event) {
+    	console.log($(this).data().link);
+
+        // if (this.hash !== "") {
+        //     // Prevent default anchor click behavior
+        //     event.preventDefault();
+
+        //     // Store hash
+        //     var hash = this.hash;
+
+        //     $('html, body').animate({
+        //         scrollTop: $(hash).offset().top
+        //     }, 800, function() {
+
+        //         window.location.hash = hash;
+        //     });
+        // }
+
+    });
+
+    // setTimeout(fadeInLinks,4000);
 
 })
